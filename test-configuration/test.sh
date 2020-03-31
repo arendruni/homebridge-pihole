a89b10be76b17150974d14405e41d672e38c1473
Andrea Giunio Bruni <arendruni@aim.com>
2020-03-31 23:21:41 +0200

    install pihole test

--- test-configuration/test.sh
+++ test-configuration/test.sh
@@ -3,6 +3,9 @@
 # install homebridge
 npm install -g --unsafe-perm homebridge
 
+# install pihole
+curl -sSL https://install.pi-hole.net | bash
+
 # test configuration
 DEBUG=* timeout --preserve-status --kill-after 30s --signal SIGINT 20s homebridge --debug --no-qrcode --user-storage-path ./test-configuration --plugin-path ./
 
