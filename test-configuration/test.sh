555d9467f4f5b582cc9ebdcd5fb7f650ad628a1b
Andrea Giunio Bruni <arendruni@aim.com>
2020-04-01 00:04:49 +0200

    fix test request

--- test-configuration/test.sh
+++ test-configuration/test.sh
@@ -3,5 +3,8 @@
 # install homebridge image
 npm install -g --unsafe-perm homebridge
 
-DEBUG=* timeout --preserve-status --kill-after 40s --signal SIGINT 30s homebridge --debug --no-qrcode --user-storage-path ./test-configuration --plugin-path ./ &
-sleep 10 && curl -s -X PUT http://localhost:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-156" --data "{\"characteristics\":[{\"aid\":3,\"iid\":10,\"value\":false}]}"
+DEBUG=* nohup timeout --preserve-status --kill-after 40s --signal SIGINT 30s homebridge --debug --insecure --no-qrcode --user-storage-path ./test-configuration --plugin-path ./ &
+sleep 10 
+curl -s -X PUT http://localhost:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-156" --data "{\"characteristics\":[{\"aid\":2,\"iid\":10,\"value\":false}]}"
+
+exit $?
