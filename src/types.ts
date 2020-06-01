fdc41fedf459fe51c587ff0c44d709202db77467
Noah Overcash <me@ncovercash.dev>
2020-06-01 06:30:45 -0400

    Add some starting types

---
+++ src/types.ts
@@ -0,0 +1,40 @@
+import {
+	AccessoryConfig
+} from "homebridge";
+
+export enum LogLevel {
+	DISABLED = 0,
+	ERROR = 1,
+	INFO = 2,
+};
+
+export interface PiHoleAccessoryConfig extends AccessoryConfig {
+	manufacturer ?: string;
+	model ?: string;
+	"serial-number" ?: string;
+	auth ?: string;
+	ssl ?: boolean;
+	host ?: string;
+	baseDirectory ?: string;
+	time ?: number;
+	port ?: number;
+	logLevel ?: LogLevel;
+
+};
+
+export type PiHoleRequest<Endpoint extends string> = {
+	[key in Endpoint]: any; // usually a "1" or current time, argument does not matter
+};
+
+export type PiHoleAuthenticatedRequest<Endpoint> = PiHoleRequest<Endpoint> & {
+	auth: string
+};
+
+// from https://discourse.pi-hole.net/t/pi-hole-api/1863
+export type PiHoleStatusRequest = PiHoleRequest<"status">;
+export type PiHoleEnableRequest = PiHoleAuthenticatedRequest<"enable">;
+export type PiHoleDisableRequest = PiHoleAuthenticatedRequest<"disable">;
+
+export type PiHoleStatusResponse = {
+	status: "disabled" | "enabled";
+};
