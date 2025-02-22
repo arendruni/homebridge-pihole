deefc9439eeaef6165207a80f99b8785a5d9cb38
Andrea Giunio Bruni <dev@giun.io>
2025-02-22 18:20:03 +0100

    ci: test on homebridge beta

--- test-configuration/test.sh
+++ test-configuration/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install homebridge
-npm install -g --unsafe-perm homebridge
+npm install -g --unsafe-perm homebridge@${HOMEBRIDGE_VERSION-latest}
 
 # test configuration
 DEBUG=* timeout --preserve-status --kill-after 30s --signal SIGINT 20s homebridge --debug --no-qrcode --user-storage-path ./test-configuration --plugin-path ./
